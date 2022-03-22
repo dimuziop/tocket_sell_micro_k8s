07a883f34498e7cfe59f0c7663e6f84b6d78fc09
pat <dimuziop@gmail.com>
2022-03-22 09:17:05 +0100

    feature: aading user creation minnimum imp

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,25 +1,39 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import express, {Request, Response} from 'express';
+import {body, validationResult} from 'express-validator';
+import {RequestValidationError} from '../errors/request-validation-error';
+import {User} from "../models/user";
+import {BadRequestError} from "../errors/bad-request-error";
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
-  body('email').isEmail().withMessage("Email must be valid"),
-  body('password').trim().isLength({ min: 6, max: 20 }).withMessage("Password must be between 6 and 20 chars"),
-], (req: Request, res: Response) => {
-  const errors = validationResult(req);
+    body('email')
+        .isEmail()
+        .withMessage("Email must be valid"),
+    body('password').trim()
+        .isLength({min: 6, max: 20})
+        .withMessage("Password must be between 6 and 20 chars"),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
-  console.info("Creating a user.....")
-  throw new DatabaseConnectionError();
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+    console.info("Creating a user.....")
 
+    const {email, password} = req.body;
 
-  const { email, password } = req.body;
-  res.send('Hi there!')
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+        console.info("Email in use")
+        throw new BadRequestError("Try again with another account")
+    }
+
+    const user = User.build({email, password});
+    await user.save();
+
+    res.status(201).send(user);
 })
 
-export { router as signUpRouter }
+export {router as signUpRouter}
