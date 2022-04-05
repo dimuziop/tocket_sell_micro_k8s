b0bd3be7d38f1c86e4b4c6a32d4d4d86e7563a94
pat <dimuziop@gmail.com>
2022-04-05 15:39:58 +0200

    chore: refactoring validation error middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body, validationResult} from "express-validator";
+import {RequestValidationError} from "../errors/request-validation-error";
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!')
-})
+router.post('/api/users/signin', [
+        body('email')
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body('password').trim().notEmpty()
+            .withMessage("Password must be present"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
 
-export { router as signInRouter }
+        res.send('Hi there!')
+    })
+
+export {router as signInRouter}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -4,44 +4,42 @@ import {RequestValidationError} from '../errors/request-validation-error';
 import jwt from 'jsonwebtoken';
 import {User} from "../models/user";
 import {BadRequestError} from "../errors/bad-request-error";
+import {validateRequest} from "../middlewares/validate-request";
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage("Email must be valid"),
-    body('password').trim()
-        .isLength({min: 6, max: 20})
-        .withMessage("Password must be between 6 and 20 chars"),
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+        body('email')
+            .isEmail()
+            .withMessage("Email must be valid"),
+        body('password').trim()
+            .isLength({min: 6, max: 20})
+            .withMessage("Password must be between 6 and 20 chars"),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        console.info("Creating a user.....")
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-    console.info("Creating a user.....")
+        const {email, password} = req.body;
 
-    const {email, password} = req.body;
+        const existingUser = await User.findOne({email})
 
-    const existingUser = await User.findOne({email})
+        if (existingUser) {
+            console.info("Email in use")
+            throw new BadRequestError("Try again with another account")
+        }
 
-    if (existingUser) {
-        console.info("Email in use")
-        throw new BadRequestError("Try again with another account")
-    }
+        const user = User.build({email, password});
+        await user.save();
 
-    const user = User.build({email, password});
-    await user.save();
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
 
-    const userJwt = jwt.sign({
-        id: user.id,
-        email: user.email
-    }, process.env.JWT_KEY!);
+        req.session = {jwt: userJwt};
 
-    req.session = {jwt: userJwt};
-
-    res.status(201).send(user);
-})
+        res.status(201).send(user);
+    })
 
 export {router as signUpRouter}
