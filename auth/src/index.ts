a3325795c77b2f94e23190be1d0dd8ec92d3e582
pat <dimuziop@gmail.com>
2022-04-05 11:13:15 +0200

    feature: managing secrets with K8S and auth token

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,6 +33,11 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be declared")
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("connected to DB")
